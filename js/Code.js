9d306bad126c6d4f1c6274961cb2577519de5661
zary018 <73082179+zary018@users.noreply.github.com>
2021-04-25 12:52:39 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,80)
+
+    fill("white")
+    textSize(15)
+    text("N O F I T N U C", 700,150)
+    fill("lightblue")
+    text("Hint: To perform a certain task !!", 700,180)
+
+    fill("white")
+    textSize(15)
+    text("S D A E B A A T", 100,350)
+    fill("lightblue")
+    text("Hint: Stores all the information !!", 100,380)
+
+    
+
+}
